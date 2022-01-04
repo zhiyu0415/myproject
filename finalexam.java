c598e75f4a49c1a7cf5ff7de4f0f5cc95d0d0f90
zhiyu0415 <93966100+zhiyu0415@users.noreply.github.com>
2022-01-04 15:11:51 +0800

    Add files via upload

---
+++ finalexam.java
@@ -0,0 +1,17 @@
+package com.fish;
+
+import java.util.Scanner;
+
+public class finalexam {
+    public static void main(String[] args) {
+        int n=-1;
+        Scanner scanner=new Scanner(System.in);
+        while (n!=0){
+            System.out.println("buy items:");
+            System.out.println("1)Dress");
+            System.out.println("2)Box");
+            System.out.println("3)Bag");
+            System.out.println("0)Exit");
+        }
+    }
+}
